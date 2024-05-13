dca7e7d87c5a14b528c38562334d1c0e95d90dee
Yatii2 <37732@ma-web.nl>
2024-05-13 12:09:55 +0200

    03

--- ConsoleMonGame/ConsoleMon.cs
+++ ConsoleMonGame/ConsoleMon.cs
@@ -2,8 +2,17 @@
 {
     internal class ConsoleMon
     {
-        int health;
-        int energy;
-        string name;
+        internal int health;
+        internal int energy;
+        internal string name;
+
+        internal void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy = energy;
+        }
     }
 }
--- ConsoleMonGame/Program.cs
+++ ConsoleMonGame/Program.cs
@@ -2,10 +2,24 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             ConsoleMon Console = new ConsoleMon();
             Skill skill = new Skill();
+            TestConsoleMonFunctions();
         }
+        static void TestConsoleMonFunctions()
+        {
+            Console.WriteLine("TestConsoleMonFunctions");
+            ConsoleMon mon = new ConsoleMon();
+            mon.TakeDamage(100);
+            mon.DepleteEnergy(20);
+
+            Console.WriteLine(mon.health == -100);
+
+            Console.WriteLine(mon.energy == -20);
+        }
+
     }
 }
