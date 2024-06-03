af9513f404cf41069dc5022ea426765d407ff836
Yatii2 <37732@ma-web.nl>
2024-06-03 09:46:04 +0200

    06

---
+++ ListOpdracht/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> birds = new List<string> { "uil", "kraai", "hond", "papegaai" };
+
+            List<string> birds2 = new List<string> { "meeuw", "duif" };
+
+            birds.AddRange(birds2);
+
+            birds.Remove("hond");
+
+            foreach (string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+        }
+    }
+}
